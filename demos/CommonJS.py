0dfc2a3a3930a0d894508d33f3a4c75f6809ae0b
kooksee <kooksee@163.com>
2015-12-05 23:13:31 +0800

    change CommonJS

--- demos/CommonJS.py
+++ demos/CommonJS.py
@@ -1,44 +1,45 @@
-#!/usr/bin/env python
+# encoding=utf-8
+# !/usr/bin/env python
 import os
-import re
 import platform
-from PyV8 import JSContext, JSError
-
 import traceback
 import logging
 
 import PyV8
 
-__author__ = "flier.lu@gmail.com"
-__version__ = "%%prog 0.1 (Google v8 engine v%s)" % PyV8.JSEngine.version
-
 
 class CommonJS(PyV8.JSClass):
-    def __init__(self, parent=None):
+    def __init__(self):
+        '''
+        _js_modules: javascript模块,为加载
+        _loaded_modules:  javascript模块,已经加载
+        _js_path: javascript脚本路径
+        '''
         super(CommonJS, self).__init__()
 
         self._js_modules = {}
         self._loaded_modules = {}
-        self.parent = parent
-        self._js_path = [(os.path.dirname(__file__), 0)]
+        self._js_path = [[os.path.dirname(__file__), False]]
 
         self.exports = {}
 
         self.__load_modules()
 
     def __load_modules(self):
-        for i in range(len(self._js_path) - 1):
+        for i in range(len(self._js_path)):
             if self._js_path[i][1]:
                 continue
 
             for (root, dirs, files) in os.walk(self._js_path[i][0]):
+                print files,'111111111',dirs,root
+
                 for _file in files:
                     namespace, ext = os.path.splitext(_file)
                     if ext != ".js":
                         continue
                     self._js_modules.update(
                         {namespace: os.path.join(root, _file)})
-                    self._js_path[i][1] = 1
+                    self._js_path[i][1] = True
 
     def require(self, module):
         if not self._js_modules.has_key(module):
@@ -47,22 +48,21 @@ class CommonJS(PyV8.JSClass):
 
         logging.info("loading module <%s>...", module)
         path = self._js_modules[module]
-        if not self._loaded_modules.has_key(path):
-            with open(path, 'r') as f:
-
-                try:
-                    code = f.read().decode('utf-8')
-                    if platform.system() == 'Windows':
-                        code = code.encode('utf-8')
-                    with PyV8.JSContext(self) as ctxt:
-                        ctxt.eval(code)
-                except:
-                    logging.debug(traceback.extract_stack())
-
-                self._loaded_modules[path] = ctxt.locals.exports
-                return self._loaded_modules[path]
-        else:
+        if self._loaded_modules.has_key(path):
             return self._loaded_modules[path]
+
+        with open(path, 'r') as f:
+            try:
+                code = f.read().decode('utf-8')
+                if platform.system() == 'Windows':
+                    code = code.encode('utf-8')
+                with PyV8.JSContext(self) as ctxt:
+                    ctxt.eval(code)
+                    self._loaded_modules[path] = ctxt.locals.exports
+                return self._loaded_modules[path]
+            except:
+                logging.debug(traceback.extract_stack())
+                return
 
     def append(self, path):
 
@@ -70,7 +70,7 @@ class CommonJS(PyV8.JSClass):
             if path == _path:
                 break
         else:
-            self._js_path.append((path, 0))
+            self._js_path.append((path, False))
             self.__load_modules()
 
         return self
@@ -91,9 +91,12 @@ class CommonJS(PyV8.JSClass):
             except:
                 logging.error("fail to process debug event")
                 logging.debug(traceback.extract_stack())
+                return
 
 
 if __name__ == '__main__':
     ctx = CommonJS()
-    # ctx.append('/my/js/path')
+    # ctx.append('hello')
+    print ctx._js_path
+    print ctx._js_modules
     print ctx.execute('require("math");')
